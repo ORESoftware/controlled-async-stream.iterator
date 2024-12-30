e1352559239825be00bb678574dbe3eff1351901
Alex.Mills <alexander.d.mills@gmail.com>
2024-12-29 23:24:12 -0500

    obs

--- src/sync-sequence/sync-sequence.ts
+++ src/sync-sequence/sync-sequence.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env pnpx tsx
 
-import {Observable} from "rxjs";
+import { Observable, from, zip as rxZip, concat as rxConcat } from 'rxjs';
+import { map as rxMap, filter as rxFilter, take as rxTake, skip as rxSkip, tap as rxTap } from 'rxjs/operators';
 
 class SyncSequence<T> {
   private iterable: Iterable<T>;
@@ -11,12 +12,6 @@ class SyncSequence<T> {
 
   static from<T>(g: () => Iterable<T>): SyncSequence<T> {
     return new SyncSequence<T>(g());
-  }
-
-  static fromObservable<T>(observable: Observable<T>): SyncSequence<T> {
-    const items: T[] = [];
-    observable.subscribe((value) => items.push(value));
-    return SyncSequence.from(() => items);
   }
 
   // Map method
@@ -34,17 +29,43 @@ class SyncSequence<T> {
     return reduce(this.iterable, reducer, initialValue);
   }
 
-  flatMap<U>(fn: (item: T) => Iterable<U>): SyncSequence<U> {
-    return new SyncSequence(flatMap(this.iterable, fn));
+  // Take method
+  take(n: number): SyncSequence<T> {
+    return new SyncSequence(take(this.iterable, n));
   }
 
-  // Make LazySequence iterable
+  // Skip method
+  skip(n: number): SyncSequence<T> {
+    return new SyncSequence(skip(this.iterable, n));
+  }
+
+  // Concat method
+  concat(...sequences: SyncSequence<T>[]): SyncSequence<T> {
+    return new SyncSequence(concat(this.iterable, ...sequences.map((seq) => seq.iterable)));
+  }
+
+  // Zip method
+  zip<U>(other: SyncSequence<U>): SyncSequence<[T, U]> {
+    return new SyncSequence(zip(this.iterable, other.iterable));
+  }
+
+  // Tap method
+  tap(fn: (item: T) => void): SyncSequence<T> {
+    return new SyncSequence(tap(this.iterable, fn));
+  }
+
+  // RxJS interop: convert to Observable
+  toObservable(): Observable<T> {
+    return from(this.iterable);
+  }
+
+  // Make SyncSequence iterable
   [Symbol.iterator](): Iterator<T> {
     return this.iterable[Symbol.iterator]();
   }
 }
 
-// Helper functions for map, filter, and reduce
+// Helper functions
 function* map<T, U>(iter: Iterable<T>, fn: (item: T) => U): IterableIterator<U> {
   for (const item of iter) {
     yield fn(item);
@@ -56,13 +77,6 @@ function* filter<T>(iter: Iterable<T>, predicate: (item: T) => boolean): Iterabl
     if (predicate(item)) {
       yield item;
     }
-  }
-}
-
-// Helper function for flatMap
-function* flatMap<T, U>(iter: Iterable<T>, fn: (item: T) => Iterable<U>): IterableIterator<U> {
-  for (const item of iter) {
-    yield* fn(item); // Flatten the iterable returned by `fn`
   }
 }
 
@@ -78,6 +92,70 @@ function reduce<T, U>(
   return accumulator;
 }
 
+function* take<T>(iter: Iterable<T>, n: number): IterableIterator<T> {
+  let count = 0;
+  for (const item of iter) {
+    if (count++ < n) yield item;
+    else break;
+  }
+}
+
+function* skip<T>(iter: Iterable<T>, n: number): IterableIterator<T> {
+  let count = 0;
+  for (const item of iter) {
+    if (count++ >= n) yield item;
+  }
+}
+
+function* concat<T>(...iters: Iterable<T>[]): IterableIterator<T> {
+  for (const iter of iters) {
+    yield* iter;
+  }
+}
+
+function* zip<T, U>(iter1: Iterable<T>, iter2: Iterable<U>): IterableIterator<[T, U]> {
+  const it1 = iter1[Symbol.iterator]();
+  const it2 = iter2[Symbol.iterator]();
+  let a = it1.next();
+  let b = it2.next();
+
+  while (!a.done && !b.done) {
+    yield [a.value, b.value];
+    a = it1.next();
+    b = it2.next();
+  }
+}
+
+function* tap<T>(iter: Iterable<T>, fn: (item: T) => void): IterableIterator<T> {
+  for (const item of iter) {
+    fn(item);
+    yield item;
+  }
+}
+
+// Example usage
+const sequence = SyncSequence.from(() => [1, 2, 3, 4, 5])
+  .map((x) => x * 2)
+  .filter((x) => x > 5)
+  .take(3)
+  .skip(1)
+  .tap((x) => console.log('Tapped:', x));
+
+for (const item of sequence) {
+  console.log(item); // Processed output
+}
+
+// RxJS interop example
+const observable = sequence.toObservable().pipe(
+  rxMap((x) => x * 2),
+  rxFilter((x) => x > 10),
+  rxTake(2),
+  rxSkip(1),
+  rxTap((x) => console.log('RxJS tapped:', x))
+);
+
+observable.subscribe((x) => console.log('RxJS:', x));
+
 
 // Generator function that produces a range of numbers
 function* range(start: number, end: number): IterableIterator<number> {
@@ -87,11 +165,11 @@ function* range(start: number, end: number): IterableIterator<number> {
 }
 
 // Use the generator function with LazySequence
-const sequence = SyncSequence.from(() => range(1, 10)) // [1, 2, 3, ..., 10]
+const sequence2 = SyncSequence.from(() => range(1, 10)) // [1, 2, 3, ..., 10]
   .map((x) => x * 2)           // Multiply each number by 2
   .filter((x) => x > 10);      // Keep only numbers greater than 10
 
 // Iterate over the sequence with for...of
-for (const item of sequence) {
+for (const item of sequence2) {
   console.log(item); // Output: 12, 14, 16, 18, 20
 }
